ca4becf16a0f07ff8f752bee17ab32309b7c75ce
joon <351117803@qq.com>
2020-01-14 20:03:11 +0800

    完成全屏功能

--- src/components/basic-layout/header-main/index.jsx
+++ src/components/basic-layout/header-main/index.jsx
@@ -1,22 +1,48 @@
 import React, { Component } from "react";
 import { Button, Icon, Layout } from "antd";
+import screenfull from "screenfull";
 
 import "./index.less";
 
 const { Header } = Layout;
 
 export default class HeaderMain extends Component {
+  state = {
+    isScreenFull: false
+  };
+
+  componentDidMount() {
+    screenfull.on("change", this.handleScreenFull);
+  }
+
+  componentWillUnmount() {
+    screenfull.off("change", this.handleScreenFull);
+  }
+
+  handleScreenFull = () => {
+    this.setState({
+      isScreenFull: !this.state.isScreenFull
+    });
+  };
+
+  screenFull = () => {
+    screenfull.toggle();
+  };
+
   render() {
+    const { isScreenFull } = this.state;
     return (
       <Header
         style={{ background: "#fff", padding: 0 }}
         className="layout-head"
       >
         <div className="layout-head-top">
-          <Button size="small">
-            <Icon type="fullscreen" />
+          <Button size="small" onClick={this.screenFull}>
+            <Icon type={isScreenFull ? "fullscreen-exit" : "fullscreen"} />
           </Button>
-          <Button size="small" className='layout-head-lang'>English</Button>
+          <Button size="small" className="layout-head-lang">
+            English
+          </Button>
           <span>hello, admin</span>
           <Button size="small" type="link">
             退 出
